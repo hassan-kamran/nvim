2cc4ab22ccc156862f0821fa17985ec3f1a7160c
Hassan Kamran <152590873+hassan-owr@users.noreply.github.com>
2025-01-26 06:44:38 +0500

    Added Icons

--- init.lua
+++ init.lua
@@ -266,12 +266,36 @@ require("lazy").setup({
 	-- Telescope
 	{
 		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- Add this line
+		},
 		keys = {
 			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
 			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
 			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
 		},
+		config = function() -- Add this config block
+			require("telescope").setup({
+				defaults = {
+					prompt_prefix = "   ",
+					selection_caret = "  ",
+					entry_prefix = "   ",
+					initial_mode = "insert",
+					path_display = { "smart" },
+					layout_config = {
+						horizontal = { prompt_position = "top" },
+					},
+					file_ignore_patterns = { "node_modules", ".git" },
+					mappings = {
+						i = {
+							["<C-j>"] = "move_selection_next",
+							["<C-k>"] = "move_selection_previous",
+						},
+					},
+				},
+			})
+		end,
 	},
 
 	-- Autocompletion
